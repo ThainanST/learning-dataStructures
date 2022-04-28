3aac7dd4cce91b7333d28a11274b19bba4b2180b
ThainanST <66067872+ThainanST@users.noreply.github.com>
2022-04-28 11:25:37 -0300

    done(firstClass):ok (#1)

---
+++ class01_helloWorld.cpp
@@ -0,0 +1,10 @@
+
+// http://www.each.usp.br/digiampietri/ed/
+// https://code.visualstudio.com/docs/languages/cpp
+
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    return 0;
+}
